eadabce1758a4721f44885db034e93d9b0a14392
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-18 16:25:05 +0200

    Add db and movies index and bootstrap

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import nunjucks from 'nunjucks';
 import fs from 'fs';
+import { Sequelize, DataTypes } from 'sequelize';
 const app = express();
 const port = 3000;
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db.sqlite'
+});
+
+const Movie = db.define('Movie', {
+  name: DataTypes.TEXT,
+  length: DataTypes.INTEGER,
+  description: DataTypes.TEXT,
+},
+{
+  timestamps: false
+});
+
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -40,6 +56,13 @@ app.get('/squareanswer', (req, res) => {
   };
   res.render('squareanswer.njk', answers);
 });
+
+app.get('/movies', async (req, res) => {
+  let movies = await Movie.findAll();
+  
+  res.render('movies/index.njk', { movies });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
